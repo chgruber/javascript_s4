ca9c13e7f40c37718e9f0f9bc3d6ebd1f2174ef5
Gruber <csap3389@uibk.ac.at>
2018-04-11 17:29:01 +0200

    erste Sicherung

---
+++ main.js
@@ -0,0 +1,33 @@
+// alert("hallo Welt");
+
+let nachricht = "Hallo Welt";
+//alert(nachricht);
+nachricht = "neuer Text";
+//alert(nachricht); 
+console.log(nachricht);
+const linkColor ="#ff0000";
+let eine_variable = "test";
+console.log("eine_variable", eine_variable);
+// infos direkt an die Variabel anhaengen
+
+
+let highScore = 10002551
+// nummerische Zeichentyp
+
+console.log("highScore ist:", highScore /20);
+let meineNachricht ="test1";
+console.log(meineNachricht);
+meineNachricht = `Ein mehr
+und ein ganz langer Text
+und aus`;
+
+console.log(meineNachricht);
+
+let fullname = `Jeffrey "The Dude" Ledosky`
+console.log(fullname)
+
+
+let isover18 = true;
+console.log("über 18?", isover18);
+let age = 17;
+console.log("age über 18", age>18);
